860b5c58775efe5ee8de279c6eb52e9e684bc3a8
dazzle8313 <dazzle8313@gmail.com>
2026-09-17 01:43:54 +0900

    Update room_scene_view.dart

--- lib/room_scene_view.dart
+++ lib/room_scene_view.dart
@@ -4,11 +4,21 @@ import 'package:flutter/services.dart';
 
 class RoomSceneView extends StatefulWidget {
   final String usdzPath;
+
+  /// plan.json の文字列。渡すと3Dをこの内容から組み立て直す。
+  /// 空なら usdzPath のモデルをそのまま表示する。
+  final String planJson;
+
+  /// 平面図で選択中の番号。3D側で色を変える。選択なしは -1。
+  final int selNo;
+
   final ValueChanged<String>? onMessage;
 
   const RoomSceneView({
     Key? key,
     required this.usdzPath,
+    this.planJson = '',
+    this.selNo = -1,
     this.onMessage,
   }) : super(key: key);
 
@@ -30,7 +40,10 @@ class _RoomSceneViewState extends State<RoomSceneView> {
     return UiKitView(
       viewType: 'room_scene_view',
       layoutDirection: TextDirection.ltr,
-      creationParams: <String, dynamic>{'usdzPath': widget.usdzPath},
+      creationParams: <String, dynamic>{
+        'usdzPath': widget.usdzPath,
+        'planJson': widget.planJson,
+      },
       creationParamsCodec: const StandardMessageCodec(),
       onPlatformViewCreated: _onCreated,
     );
@@ -43,6 +56,25 @@ class _RoomSceneViewState extends State<RoomSceneView> {
       return null;
     });
     _channel = ch;
+    // 生成中に値が変わっていることがあるので、いまの状態を送り直す
+    _send();
+  }
+
+  @override
+  void didUpdateWidget(covariant RoomSceneView old) {
+    super.didUpdateWidget(old);
+    if (old.planJson != widget.planJson || old.selNo != widget.selNo) {
+      _send(planChanged: old.planJson != widget.planJson);
+    }
+  }
+
+  void _send({bool planChanged = true}) {
+    final ch = _channel;
+    if (ch == null) return;
+    if (planChanged && widget.planJson.isNotEmpty) {
+      ch.invokeMethod('plan', widget.planJson);
+    }
+    ch.invokeMethod('select', widget.selNo);
   }
 
   @override
